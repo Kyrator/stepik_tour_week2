35e5b9946c8377696c5ed3d2c4149599554af6bd
Александр Сырысев <C:\Users\kyrat\AppData\Roaming\The Bat!>
2022-04-09 21:34:35 +0300

    init django project 2

--- conf/settings.py
+++ conf/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-)9$hktnx3@)46+x1-&g2582x4++d_1kzyx36ql+eth=trjg!0g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'tours',
 ]
 
 MIDDLEWARE = [
